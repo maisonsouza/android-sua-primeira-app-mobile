f2f0fe0034501ee4927ec19d8102d9ab23a16bd5
Maison Galvão <maisongalvao144@gmail.com>
2019-05-28 13:21:49 -0400

    Merge branches 'aula01' and 'aula02' of https://github.com/maisonsouza/android-sua-primeira-app-mobile into aula02
    # Conflicts:
    #	app/src/main/java/com/maison/agenda/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/maison/agenda/MainActivity.java
+++ app/src/main/java/com/maison/agenda/MainActivity.java
@@ -1,11 +1,9 @@
 package com.maison.agenda;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
